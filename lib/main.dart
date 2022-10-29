f14c6648fe8f9a7b4e32115b3e1bcd35558c2131
TikuGaelle <brendagaelle6@gmail.com>
2022-10-29 11:22:49 +0100

    helloworld

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:diceapp/design.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,60 +15,52 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage( ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  
-
-  final String title;
-
+  const MyHomePage({Key? key,  }) : super(key: key);
+   
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
-
+   
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 3, 238, 214),
       appBar: AppBar(
-         
-        title: Text(widget.title),
+         backgroundColor: Colors.teal.shade500,
+         elevation: 10,
+         leading: Icon(Icons.more_vert_outlined),
+        
       ),
       body: Center(
-        
         child: Column(
-        
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+           
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: ( ){
+           Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Home()),
+                                  );
+        },
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.forward),
       ),   );
   }
 }
